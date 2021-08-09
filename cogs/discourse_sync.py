9e1add8975ff3a39dc147a393044db8eace28894
gradiuscypher <gradiuscypher@gmail.com>
2021-08-08 23:31:46 -0700

    more work

--- cogs/discourse_sync.py
+++ cogs/discourse_sync.py
@@ -1,12 +1,14 @@
 # TODO: make it so that we can sync multiple roles at a time
+# TODO IMPORTANT: we'll have to find out how to get Discord IDs, searching by user names is too flaky.
 
 import trace
-import discord
+import discord.utils
 import logging
 import traceback
 from discord.ext import tasks, commands
+from discord.mentions import A
 from pydiscourse import DiscourseClient
-from config import discourse_api_key, discourse_api_user, discourse_url
+from config import discourse_api_key, discourse_api_user, discourse_url, target_guild_id
 
 # setup logging
 client = DiscourseClient(discourse_url, discourse_api_user, discourse_api_key)
@@ -16,14 +18,23 @@ logger = logging.getLogger("gradiusbot")
 class DiscourseSync(commands.Cog):
     def __init__(self, bot) -> None:
         self.sync_roles.start()
+        self.bot = bot
 
-    @tasks.loop(seconds=300)
+    @tasks.loop(seconds=5)
     async def sync_roles(self):
         try:
-            # get the "member" discourse role ID
-            member_role_id = client.group('member')['group']['id']
+            target_guild = discord.utils.get(self.bot.guilds, id=target_guild_id)
+            if target_guild:
+                print(target_guild.name)
+                member_role_id = client.group('member')['group']['id']
 
-            # TODO: iterate over every discourse user and add roles to members who don't have them
+                user_list = client.users()
+                for user in user_list:
+                    print(f"trying {user['username']}")
+                    target_discord_user = target_guild.get_member_named(user['username'])
+
+                    if target_discord_user:
+                        print(user['username'], target_discord_user)
         except:
             logger.error(traceback.format_exc())
 
